d1631367ecd771531e9b5a6178a141bda3d8f278
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-06 16:43:57 +0100

    refactor: print errors to stderr instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process;
 
 fn main() {
     parse_single_command("P 2").unwrap_or_else(|err| {
-        println!("Problem parsing arugments: {:?}", err);
+        eprintln!("Problem parsing arugments: {:?}", err);
         process::exit(1);
     });
 }
