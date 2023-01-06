14cbea5a1ecdd7460b9c47914fb2fd3989ac2448
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-06 17:54:58 +0100

    refactor: add error handling to main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use pragmatic_programmer::parse_input_commands;
-// use std::process;
+use std::process;
 
 fn main() {
-    parse_input_commands("P 2");
-    // .unwrap_or_else(|err| {
-    //     eprintln!("Problem parsing arugments: {:?}", err);
-    //     process::exit(1);
-    // });
+    parse_input_commands("P 2").unwrap_or_else(|err| {
+        eprintln!("Problem parsing arugments: {:?}", err);
+        process::exit(1);
+    });
 }
