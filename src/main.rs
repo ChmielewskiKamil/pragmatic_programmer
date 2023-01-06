6fa251f4fc0f6c50703a6a38c25cfd37c0ddc141
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-06 13:52:28 +0100

    chore: add commands enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+enum Commands {
+    PenSelect(u32),
+    PenDown,
+    PenUp,
+    DrawWest(u32),
+    DrawEast(u32),
+    DrawNorth(u32),
+    DrawSouth(u32),
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod parsing_tests {
+    use super::Commands;
+
+    #[test]
+    fn it_should_parse_pen_select_from_str() {
+        assert_eq!(parse_command("P 2"), Commands::PenSelect(2));
+    }
 }
