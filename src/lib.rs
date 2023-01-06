ff9c02e4627cee8a7102d3c09c497213796a49f6
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-06 17:30:46 +0100

    test: add two commands parsing test

--- src/lib.rs
+++ src/lib.rs
@@ -89,4 +89,12 @@ mod user_input_parsing_tests {
     fn it_should_parse_single_command() {
         assert_eq!(parse_input_commands("N 1"), vec![Command::DrawNorth(1)]);
     }
+
+    #[test]
+    fn it_should_parse_two_commands() {
+        assert_eq!(
+            parse_input_commands("P 2\nE 5"),
+            vec![Command::PenSelect(2), Command::DrawEast(5)]
+        );
+    }
 }
