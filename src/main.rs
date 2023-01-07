7a3f37efdc085afbe548a7c23dee0d3843b50f0d
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-07 18:59:16 +0100

    chore(executable): start working on CLI app

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 // use std::process;
 // use turtle_language_parser::print_path_for_commands;
+use std::env;
 
 fn main() {
     // parse_input_commands("P 2").unwrap_or_else(|err| {
     //     eprintln!("Problem parsing arugments: {:?}", err);
     //     process::exit(1);
     // });
+
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 }
